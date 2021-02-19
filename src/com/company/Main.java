df312b32b0e63933f9a99a76e9b8368ddf7a1711
Andrzej Łuszcz <endriu1297@gmail.com>
2021-02-20 00:38:10 +0100

    add logic in calculator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+        int weight = 70;
+        double height = 1.8;
+        final double bmi;
+
+        System.out.println("Twoja waga to: " + weight + " kg");
+        System.out.println("Twój wzrost to: " + height + " cm");
+
+        bmi = Math.floor(weight / Math.pow(height, 2));
+
+        System.out.println("Twój wynik bmi to: " + bmi);
+
     }
 }
