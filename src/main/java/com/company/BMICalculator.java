a6c676ac1b426c228a2e5db8da291dacb882fcc9
Andrzej Łuszcz <endriu1297@gmail.com>
2021-02-21 23:23:34 +0100

    add invoke class, pass arguments to constructor

--- src/main/java/com/company/BMICalculator.java
+++ src/main/java/com/company/BMICalculator.java
@@ -8,26 +8,27 @@ public class BMICalculator {
 
     public BMICalculator (int weight, double height){
         this.weight = weight;
-        this.height = height;
+        this.height = height / 100;
     }
 
     public void calculateBmi(){
-        String weightInfo = MessageFormat.format("Twoja waga to: ", this.weight);
-        String heightInfo = MessageFormat.format("Twój wzrost to: ", this.height);
+        String weightInfo = MessageFormat.format("Twoja waga to: {0}", this.weight);
+        String heightInfo = MessageFormat.format("Twój wzrost to: {0}", this.height);
 
         System.out.println(weightInfo);
         System.out.println(heightInfo);
 
-        final double bmi = Math.floor(weight / height * height);
+        final double bmi = Math.floor(this.weight / (this.height * this.height));
 
-        String resultInfo = MessageFormat.format("Twój wynik bmi to: ", bmi);
+        String resultInfo = MessageFormat.format("Twój wynik bmi to: {0}", bmi);
 
         System.out.println(resultInfo);
 
     }
 
     public static void main(String[] args) {
+        BMICalculator bmiCalculator = new BMICalculator(Integer.parseInt(args[0]), Double.parseDouble(args[1]) );
 
-
+        bmiCalculator.calculateBmi();
     }
 }
