c3b6491c32802b512b3e6a79306550e79ad5b951
redjake <35747489+Reddzejk@users.noreply.github.com>
2021-02-21 12:49:50 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,21 @@ package com.company;
 public class BMICalculator {
 
     public static void main(String[] args) {
-        #Użyj args
+        //Użyj args
         int weight = 70;
-        #Uzyj args
+        //Uzyj args
         double height = 1.8;
-        # połącz deklarację z inicjalizacją - w tym wypadku tak będzie linijka mniej
+        //połącz deklarację z inicjalizacją - w tym wypadku tak będzie linijka mniej
         final double bmi;
         
-        # Zapoznaj się z klasą MessageFormat pozwoli Ci ona na interpolacje stringów a nie concat :) 
+        //Zapoznaj się z klasą MessageFormat pozwoli Ci ona na interpolacje stringów a nie concat :) 
         System.out.println("Twoja waga to: " + weight + " kg");
         System.out.println("Twój wzrost to: " + height + " cm");
 
-        #do potęgi 2 lepiej mnożyż height * height
+        //do potęgi 2 lepiej mnożyż height * height
         bmi = Math.floor(weight / Math.pow(height, 2));
 
+        //tutaj też MassageFormat będzie dobry.
         System.out.println("Twój wynik bmi to: " + bmi);
     }
 }
